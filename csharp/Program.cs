3f6d8e0d5bdcf53db68fde3e3e81f70df9352e90
Mohit-Pala <vmohit.pala@gmail.com>
2026-01-31 00:24:08 -0500

    IT WORKSSSSSS

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using DiceGame3.Enums;
+using DiceGame3.Models;
+
+var numSides = 6;
+float prob = 1.0f / numSides;
+var dieFacesList = new List<DieFace>();
+for (int i = 0; i < numSides; i++)
+{
+  dieFacesList.Add(new DieFace(DieFaceValues.One, prob));
+  dieFacesList.Add(new DieFace(DieFaceValues.Two, prob));
+  dieFacesList.Add(new DieFace(DieFaceValues.Three, prob));
+  dieFacesList.Add(new DieFace(DieFaceValues.Four, prob));
+  dieFacesList.Add(new DieFace(DieFaceValues.Five, prob));
+  dieFacesList.Add(new DieFace(DieFaceValues.Six, prob));
+}
+
+var die = new Die(dieFacesList);
+
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine(die.Roll().FaceValue);
+}
+
+
