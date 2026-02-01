b20b9c11a1bf42b4665fea313d98191ffd7d68fd
Mohit-Pala <vmohit.pala@gmail.com>
2026-02-01 02:10:39 -0500

    why ts not working

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,26 +1,35 @@
-﻿using System;
+﻿// using System;
+// using System.Collections.Generic;
+// using DiceGame3.Enums;
+// using DiceGame3.Models;
+
+// var numSides = 6;
+// float prob = 1.0f / numSides;
+// var dieFacesList = new List<DieFace>();
+// for (int i = 0; i < numSides; i++)
+// {
+//   dieFacesList.Add(new DieFace(DieFaceValues.One, prob));
+//   dieFacesList.Add(new DieFace(DieFaceValues.Two, prob));
+//   dieFacesList.Add(new DieFace(DieFaceValues.Three, prob));
+//   dieFacesList.Add(new DieFace(DieFaceValues.Four, prob));
+//   dieFacesList.Add(new DieFace(DieFaceValues.Five, prob));
+//   dieFacesList.Add(new DieFace(DieFaceValues.Six, prob));
+// }
+
+// var die = new Die(dieFacesList);
+
+// for (int i = 0; i < 10; i++)
+// {
+//   Console.WriteLine(die.Roll().FaceValue);
+// }
+
+
+using System;
 using System.Collections.Generic;
-using DiceGame3.Enums;
-using DiceGame3.Models;
 
-var numSides = 6;
-float prob = 1.0f / numSides;
-var dieFacesList = new List<DieFace>();
-for (int i = 0; i < numSides; i++)
-{
-  dieFacesList.Add(new DieFace(DieFaceValues.One, prob));
-  dieFacesList.Add(new DieFace(DieFaceValues.Two, prob));
-  dieFacesList.Add(new DieFace(DieFaceValues.Three, prob));
-  dieFacesList.Add(new DieFace(DieFaceValues.Four, prob));
-  dieFacesList.Add(new DieFace(DieFaceValues.Five, prob));
-  dieFacesList.Add(new DieFace(DieFaceValues.Six, prob));
-}
-
-var die = new Die(dieFacesList);
-
-for (int i = 0; i < 10; i++)
-{
-  Console.WriteLine(die.Roll().FaceValue);
-}
-
-
+var diceValues = new List<int> { 1, 2, 3, 4, 5, 6 };
+var result = DiceGame3.Helpers.DieScoringCalculator.CalcScore(diceValues);
+Console.WriteLine(result.Score);
+Console.WriteLine(result.AlreadyScoredDieCount);
+Console.WriteLine(result.IsBust);
+Console.WriteLine(result.CanContinueScoring(diceValues.Count));
